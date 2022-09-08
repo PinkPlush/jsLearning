128014dea86ba2c12e05b93acee2537a39e114f7
PinkPlush <raevschicatalin@gmail.com>
2022-09-08 20:19:44 +0300

    fizzbuzz 3rd try

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+let login = prompt("Enter   your    login: \n ");
+
+if (login == "Admin") {
+    let password = prompt("Enter your password: \n ");
+
+    if (password == "TheMaster") {
+        alert("Welcome");
+        }
+        else {
+            alert("try again");
+            }
+}
+else if (login == "") {
+    alert("again");
+}
+else {
+    alert("ligma")
+}
+*/
+//FizzBuzz game
+
+let number =parseInt(prompt("Enter your number: \n "));
+let initial = 1;
+
+while (number >= initial) {
+    initial++;
+    console.log(initial);
+    if(initial % 15 == 0){
+        console.log("FizzBuzz");
+    }
+    else if (initial%3 == 0){
+        console.log("Fizz");
+    }
+    else if (initial%5 == 0){
+    console.log("Buzz");
+    }
+}
+
